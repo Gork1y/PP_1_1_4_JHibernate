f0e3af9c51630bb1f6c06f57447d079a818c82f4
Kirill Gorkiy <gorkiy@list.ru>
2022-07-12 11:43:11 +0200

    добавил транзакции и роллбеки в случае ошибок запроса по задаче 1.1.3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,11 +1,14 @@
 package jm.task.core.jdbc.dao;
 
-import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
 public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
@@ -13,52 +16,83 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void createUsersTable() {
         String query = """
-                CREATE TABLE if not exists users (
-                	id bigint auto_increment NOT NULL,
-                	firstname varchar(100) NOT NULL,
-                	lastname varchar(100) NULL,
-                	age int NOT NULL,
-                	primary key (id)
-                );
+            CREATE TABLE if not exists users (
+            	id bigint auto_increment NOT NULL,
+            	firstname varchar(100) NOT NULL,
+            	lastname varchar(100) NULL,
+            	age int NOT NULL,
+            	primary key (id)
+            );
                 """;
         try (Statement statement = Util.getConnection().createStatement()) {
+            Util.getConnection().setAutoCommit(false);  //НО ВОТ НАХ оно это здесь??)))))
             statement.execute(query);
+            Util.getConnection().commit();
+
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                Util.getConnection().rollback();
+                System.out.println("JDBC Transaction rolled back successfully");
+            } catch (SQLException e1) {
+                System.out.println("SQLException in rollback" + e.getMessage());
+            }
         }
     }
 
     public void dropUsersTable() {
         String dropTableQuery = "DROP TABLE IF EXISTS users";
         try (Statement statement = Util.getConnection().createStatement()) {
+            Util.getConnection().setAutoCommit(false); //НО ВОТ НАХ оно это здесь??)))))
             statement.execute(dropTableQuery);
+            Util.getConnection().commit();
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                Util.getConnection().rollback();
+                System.out.println("JDBC Transaction rolled back successfully");
+            } catch (SQLException e1) {
+                System.out.println("SQLException in rollback" + e.getMessage());
+            }
         }
-
     }
 
     public void saveUser(String name, String lastName, byte age) {
         String saveUserQuery = "INSERT INTO users (firstname,lastname,age) VALUES (?,?,?) ";
         try (PreparedStatement preparedStatement = Util.getConnection().prepareStatement(saveUserQuery)) {
+            Util.getConnection().setAutoCommit(false); //НО ВОТ НАХ оно это здесь??)))))
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setByte(3, age);
             preparedStatement.executeUpdate();
             System.out.println("User with name " + name + " added to db");
+            Util.getConnection().commit();
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                Util.getConnection().rollback();
+                System.out.println("JDBC Transaction rolled back successfully");
+            } catch (SQLException e1) {
+                System.out.println("SQLException in rollback" + e.getMessage());
+            }
         }
-
     }
 
     public void removeUserById(long id) {
         String removeUserByIdQuery = "DELETE FROM users where id = ?";
         try (PreparedStatement preparedStatement = Util.getConnection().prepareStatement(removeUserByIdQuery)) {
+            Util.getConnection().setAutoCommit(false); //НО ВОТ НАХ оно это здесь??)))))
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
+            Util.getConnection().commit();
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                Util.getConnection().rollback();
+                System.out.println("JDBC Transaction rolled back successfully");
+            } catch (SQLException e1) {
+                System.out.println("SQLException in rollback" + e.getMessage());
+            }
         }
     }
 
@@ -67,8 +101,8 @@ public class UserDaoJDBCImpl implements UserDao {
         List<User> users = new ArrayList<>();
 
         try (Statement statement = Util.getConnection().createStatement()) {
+            Util.getConnection().setAutoCommit(false); //НО ВОТ НАХ оно это здесь??)))))
             ResultSet resultSet = statement.executeQuery(getAllUsersQuery);
-
             while (resultSet.next()) {
                 User user = new User();
                 user.setId(resultSet.getLong("id"));
@@ -77,9 +111,15 @@ public class UserDaoJDBCImpl implements UserDao {
                 user.setAge(resultSet.getByte("age"));
                 users.add(user);
             }
-
+            Util.getConnection().commit();
         } catch (Exception e) {
             e.printStackTrace();
+            try {
+                Util.getConnection().rollback();
+                System.out.println("JDBC Transaction rolled back successfully");
+            } catch (SQLException e1) {
+                System.out.println("SQLException in rollback" + e.getMessage());
+            }
         }
         return users;
     }
@@ -87,9 +127,17 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void cleanUsersTable() {
         try (Statement statement = Util.getConnection().createStatement()) {
+            Util.getConnection().setAutoCommit(false); //НО ВОТ НАХ оно это здесь??)))))
             statement.execute("TRUNCATE table users;");
+            Util.getConnection().commit();
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                Util.getConnection().rollback();
+                System.out.println("JDBC Transaction rolled back successfully");
+            } catch (SQLException e1) {
+                System.out.println("SQLException in rollback" + e.getMessage());
+            }
         }
     }
 }
